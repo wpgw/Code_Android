7ed161d8ed915e1a03e40c92d707b06d5722c3e8
philip <13986065186@139.com>
2020-05-24 10:57:54 +0800

    add Zebra print

--- base/src/main/java/com/philip/base/MainActivity.java
+++ base/src/main/java/com/philip/base/MainActivity.java
@@ -1,38 +1,31 @@
-package com.philip.wpcameral;
+package com.philip.base;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class wpCameraActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     Button btnPrint;
-    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wp_camera);
-        activity=this;
+        setContentView(R.layout.activity_main);
 
         final String item="P149WGG";
 
         btnPrint=(Button)findViewById(R.id.btnPrint);
         btnPrint.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v){
-                Toast.makeText(activity,"will output to network",Toast.LENGTH_LONG).show();
-
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -41,7 +34,7 @@ public class wpCameraActivity extends AppCompatActivity {
                             final String ip="127.0.0.1";
                             final Integer port=9100;
                             Socket client=new Socket(ip,port);
-                            String sb="^XA^LH0,0^F0203,203^BY3,2.4,50^B3N,Y,,Y^FDABC123^FS^XZ";
+                            String sb="content";
                             byte[] mybytearray=sb.getBytes();
                             OutputStream outputStream=client.getOutputStream();
                             outputStream.write(mybytearray,0,mybytearray.length);
@@ -56,8 +49,7 @@ public class wpCameraActivity extends AppCompatActivity {
                     }
                 }).start();
             }
+
         });
-
-
     }
 }
