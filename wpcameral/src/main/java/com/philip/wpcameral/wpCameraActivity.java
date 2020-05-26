8a5ff949cde2ee68cd430e2413b15317da5ee8a6
Philip Wang <pwang@meridian-mag.com>
2020-05-26 17:01:30 +0800

    zpl label output

--- wpcameral/src/main/java/com/philip/wpcameral/wpCameraActivity.java
+++ wpcameral/src/main/java/com/philip/wpcameral/wpCameraActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -16,6 +18,7 @@ import java.net.UnknownHostException;
 
 public class wpCameraActivity extends AppCompatActivity {
     Button btnPrint;
+    EditText etText;
     Activity activity;
 
     @Override
@@ -27,6 +30,8 @@ public class wpCameraActivity extends AppCompatActivity {
         final String item="P149WGG";
 
         btnPrint=(Button)findViewById(R.id.btnPrint);
+        etText=(EditText) findViewById(R.id.etText);
+        etText.setText("^XA^LH203,203^F0203,203^BY3,2.4,50^B3N,Y,,Y^FDABC123^FS^XZ");
         btnPrint.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -38,10 +43,11 @@ public class wpCameraActivity extends AppCompatActivity {
                     public void run() {
                         try{
                             Looper.prepare();
-                            final String ip="127.0.0.1";
+                            final String ip="10.70.30.162";
                             final Integer port=9100;
                             Socket client=new Socket(ip,port);
-                            String sb="^XA^LH0,0^F0203,203^BY3,2.4,50^B3N,Y,,Y^FDABC123^FS^XZ";
+                            //String sb="^XA^LH203,203^F0203,203^BY3,2.4,50^B3N,Y,,Y^FDABC123^FS^XZ";
+                            String sb=etText.getText().toString();
                             byte[] mybytearray=sb.getBytes();
                             OutputStream outputStream=client.getOutputStream();
                             outputStream.write(mybytearray,0,mybytearray.length);
