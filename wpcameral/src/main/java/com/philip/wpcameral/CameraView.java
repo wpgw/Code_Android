48559147f3508c8185f37f000172a50ddcbc1654
philip <13986065186@139.com>
2020-06-02 00:31:02 +0800

    added camera function

--- wpcameral/src/main/java/com/philip/wpcameral/CameraView.java
+++ wpcameral/src/main/java/com/philip/wpcameral/CameraView.java
@@ -1,9 +1,17 @@
 package com.philip.wpcameral;
 
+import com.philip.wpcameral.util.BitmapUtil;
+import com.philip.wpcameral.util.DateUtil;
+import com.philip.wpcameral.util.CameraUtil;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.text.format.DateUtils;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -12,49 +20,90 @@ public class CameraView extends SurfaceView {
     private Camera mCamera;
     private boolean isPreviwing=false;
     private Point mCameraSize;
-    public String mPhotoPath;
+    public String mPhotoPath;   //可返回照片保存的路径
+    private Context mContext;
 
     public CameraView(Context context, AttributeSet attrs){
         super(context,attrs);
-        //mContext=context;
+        mContext=context;
         SurfaceHolder holder=getHolder();
         holder.addCallback(mSurfaceCallback);
         holder.setFormat(PixelFormat.TRANSPARENT);
     }
 
     public void doTakePicture(){
-        mCamera.takePicture(mShutterCallback,null,mPictureCallback);
+        if(isPreviwing&&mCamera!=null){
+            mCamera.takePicture(mShutterCallback,null,mPictureCallback);
+        }
     }
-
-
 
     private Camera.ShutterCallback mShutterCallback=new Camera.ShutterCallback(){
         @Override
         public void onShutter() {
+            System.out.println("at ShutterCalback.");
         }
     };
 
     private Camera.PictureCallback mPictureCallback=new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
-
+            Bitmap raw=null;
+            if(data!=null){
+                raw= BitmapFactory.decodeByteArray(data,0,data.length);
+                mCamera.stopPreview();
+                isPreviwing=false;
+            }
+            Bitmap bitmap=BitmapUtil.getRotateBitmap(raw,90);
+            mPhotoPath=String.format("%s%s.jpg",BitmapUtil.getCachePath(mContext), DateUtil.getNowDateTime());
+            BitmapUtil.saveBitmap(mPhotoPath,bitmap,"jpg",80);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mCamera.startPreview();
+            isPreviwing=true;
         }
     };
 
     private SurfaceHolder.Callback mSurfaceCallback=new SurfaceHolder.Callback() {
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
+            mCamera=Camera.open(0);  //0 refers to behind camera, 1 refers to front camera
+            try{
+                mCamera.setPreviewDisplay(holder);
+                mCameraSize=CameraUtil.getCameraSize(mCamera.getParameters(),CameraUtil.getSize(mContext));
+                Camera.Parameters parameters=mCamera.getParameters();
+                parameters.setPreviewSize(mCameraSize.x,mCameraSize.y);
+                parameters.setPictureSize(mCameraSize.x,mCameraSize.y);
+                parameters.setPictureFormat(ImageFormat.JPEG);
+                //只有后摄像头可自动对焦
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                mCamera.setParameters(parameters);
+
+            }catch(Exception e){
+                e.printStackTrace();
+                mCamera.release();
+                mCamera=null;
+            }
 
         }
 
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+            mCamera.setDisplayOrientation(90);
+            mCamera.startPreview();
+            isPreviwing=true;
+            mCamera.autoFocus(null);
+            //mCamera.setPreviewCallback(mPreviwCallback);  //连拍用的
         }
 
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
-
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera=null;
         }
     };
 }
---
+++ wpcameral/src/main/java/com/philip/wpcameral/TakePictureActivity.java
@@ -0,0 +1,40 @@
+package com.philip.wpcameral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class TakePictureActivity extends AppCompatActivity implements OnClickListener {
+    private CameraView camera_view;
+    private int mTaketype=0; // 拍照类型。0为单拍，1为连拍
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+
+        camera_view=findViewById(R.id.camera_view);
+
+        findViewById(R.id.btn_shutter).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btn_shutter){
+            camera_view.doTakePicture();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(TakePictureActivity.this,"已完成拍照:"+camera_view.mPhotoPath,Toast.LENGTH_LONG).show();
+                }
+            },1500);
+
+        }
+    }
+}
