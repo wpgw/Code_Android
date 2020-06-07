cf6bb14be4fea8ed731ca1e16e17a630ab416f5e
philip <13986065186@139.com>
2020-06-07 22:41:40 +0800

    add system camera

--- wpcameral/src/main/java/com/philip/wpcameral/SystemCamera.java
+++ wpcameral/src/main/java/com/philip/wpcameral/SystemCamera.java
@@ -1,11 +1,17 @@
+//https://www.jianshu.com/p/eca7335602c1
+//https://www.cnblogs.com/io1024/p/11590382.html
+
 package com.philip.wpcameral;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -42,32 +48,30 @@ public class SystemCamera extends AppCompatActivity {
 
     public void playPhoto(View view){
         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Uri fileUri=Uri.fromFile(photoFile);
+        Uri fileUri=this.getUriForFile(this,photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
         startActivityForResult(intent,100);
     }
 
-    //@Override
+    @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent intent){
         super.onActivityResult(requestCode, resultCode, intent);
-        show_img(localPath);
+        System.out.println("嘿嘿  ："+requestCode);
+        if(resultCode==RESULT_OK) {
+            show_img(localPath);
+        }
     }
 
     private void show_img(String filePath){
         FileInputStream fis=null;
         try{
-            System.out.println("文件路径----------"+filePath);
-            System.out.println("文件路径----------"+filePath);
-            Log.d("记录Local Path：",filePath);
-            Log.d("记录Local Path：",filePath);
+            Log.d("记录Local Path",filePath);
 
             fis=new FileInputStream(filePath);
-
             Bitmap bitmap= BitmapFactory.decodeStream(fis);
             ivMyphoto.setImageBitmap(bitmap);
 
         }catch(Exception e){
-            System.out.println("___________Read Error__________");
             e.printStackTrace();
         }finally{
             if(fis!=null) {
@@ -78,5 +82,16 @@ public class SystemCamera extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private Uri getUriForFile(Context context, File file) {
+        Uri fileUri;
+        if (Build.VERSION.SDK_INT >= 24) {  //7.0以上
+            //参数：authority 需要和清单文件Mannifest中配置的保持完全一致：${applicationId}.fileprovider
+            fileUri = FileProvider.getUriForFile(context,"com.philip.wpcameral.fileProvider" , file);
+        } else {
+            fileUri = Uri.fromFile(file);
+        }
+        return fileUri;
     }
 }
