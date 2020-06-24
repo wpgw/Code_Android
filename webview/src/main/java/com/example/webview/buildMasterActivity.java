2d55e3917e84e19360aec6a62b3fd7bcb0db0a10
Philip Wang <pwang@meridian-mag.com>
2020-06-24 09:14:29 +0800

    begin master label function

---
+++ webview/src/main/java/com/example/webview/buildMasterActivity.java
@@ -0,0 +1,14 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buildMasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_master);
+    }
+}
---
+++ webview/src/main/java/com/example/webview/mobileLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mobileLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_login);
+    }
+}
